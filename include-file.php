22427f55640c62fcf0e524e15a0a0e5e587e4372
tstafford <tstafford@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-01 16:12:43 +0000

    Added version number
    git-svn-id: https://plugins.svn.wordpress.org/include-file/trunk@431837 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- include-file.php
+++ include-file.php
@@ -3,6 +3,7 @@
 Plugin Name: include-file
 Plugin URI: http://wordpress.org/extend/plugins/include-file/
 Description: Includes the contents of a file at a given url in a post.
+Version: 1
 Author: Tyler Stafford <tyler.stafford@gmail.com>
 Author URI: http://tylerstafford.net
 */
